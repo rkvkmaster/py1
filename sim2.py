16b4e7ccf89003cb26b320456a9769aa3e06f2e4
rkvk <rukavichkina@tut.by>
2023-03-16 17:53:27 +0300

    sim2.py

---
+++ sim2.py
@@ -0,0 +1,23 @@
+# n = int(input('введите колличество монет'))
+# k = 0
+# for i in range(n):
+#     v = int(input())
+#     if v == 1:
+#         k += 1
+# print(k if k<n/2 else n-k)
+
+import math
+sum = int(input('x+y= '))
+piece = int(input('x*y= '))
+for x < 1000 > y
+    if (x + y = sum) and (x * y = piece) then
+print('x= 'x, 'y= 'y); 
+---------?????????????
+
+
+# n = int(input("Введите число: "))
+# m = 1
+# while m < n:
+#     print(m, end=' ')
+#     m = m * 2
+
--- sim3.py
+++
@@ -1,7 +0,0 @@
-s = str(input())
-sum1=int(s[0])+int(s[1])+int(s[2])
-sum2=int(s[3])+int(s[4])+int(s[5])
-if sum1==sum2:
-  print('Счастливый')
-else:
-  print('Обычный')
